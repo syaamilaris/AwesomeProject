66beccc90c0d9af47bd663ce5cb18b6ea561c936
Muaz Zul <idmuazzulkarnain@gmail.com>
2022-10-21 01:30:46 +0800

    Added some text

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first try on making a mobile apps using react native :D</Text>
-      <Text>TEST LINE</Text>
+      <Text>
+        This is my first try on making a mobile apps using react native :D
+      </Text>
+      <Text>Wait for the awesome contents later!</Text>
       <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 250, height: 250 }}
-        />
+        source={{
+          uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+        }}
+        style={{ width: 250, height: 250 }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -20,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
